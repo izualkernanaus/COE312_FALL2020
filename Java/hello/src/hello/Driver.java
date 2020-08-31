cd565b0e511689a140148466f0559a3da872605c
izualkernan <izualkernan@gmail.com>
2020-08-31 10:26:44 +0400

    August 31 class update

--- Java/hello/src/hello/Driver.java
+++ Java/hello/src/hello/Driver.java
@@ -1,10 +1,24 @@
+
+// kind of similar to using namespace
+// this name is up to you. 
+
 package hello;
+
+// Note that name of the class (i.e., Driver) needs to be the same
+// as the name of the file Driver.java.
+
+/*
+ * 
+ This is my first Java program. 
+ * 
+ */
 
 public class Driver {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello COE312!");
+		System.out.println("Hello COE312 I am sleepy!");
+		
 
 	}
 
--- Java/variables/src/variables/Driver.java
+++ Java/variables/src/variables/Driver.java
@@ -2,23 +2,32 @@ package variables;
 
 public class Driver {
 
-	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		
 		
 		// primitive variables
+		// these start with "small letters"
+		// behave like C++ except
+		// cannot have pointers to primitive variables
+		
 		int x = 10;
 		double y = 20.0;
 		System.out.println(x);
+		System.out.println(y);
 		
+		// int *p;  //cannot do this
 		
+		// However, everything is a pointer in Java
+		// EXCEPT primitive variables. 
+		// Everything else is a Class variable.
+	    
+		Integer i;   // same as int * i;
 		
-		//class variables
-		Integer i;
-		i = new Integer(10);
+		i = new Integer(100); // i = new int(100)
 		
-		System.out.println(i.getClass());
+		System.out.println(i);
 		
+		/*
 		//converting between class and primitive variables
 		
 	
@@ -26,6 +35,7 @@ public class Driver {
 		i = Integer.valueOf("231");
 		i = Integer.parseInt("123");
 		
+		*/
 
 	}
 
