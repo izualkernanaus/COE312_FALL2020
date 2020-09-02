8dfd4a8d011edabc0db833ae77251cc2c6d6b749
izualkernan <izualkernan@gmail.com>
2020-09-02 10:23:11 +0400

    Sept 2 class

--- Java/strings/src/strings/Driver.java
+++ Java/strings/src/strings/Driver.java
@@ -4,11 +4,34 @@ public class Driver {
 
 	public static void main(String[] args) {
 		
-		String s = "how are you?";
-		System.out.println(s.contains("me"));
-		System.out.println(s.charAt(3));
+		String s = null;  // because s is an address of a string
+		System.out.println(s);
+		
+		s = "hello how are you?";
+	
+		System.out.println(s);
 		
 		
+		
+		String txt = "Hello World";
+		System.out.println(txt.toUpperCase());   // Outputs "HELLO WORLD"
+		System.out.println(txt.toLowerCase());   // Outputs "hello world"
+		
+		
+		System.out.println(s.charAt(4));
+	
+		// there is an "are" in the string
+		System.out.println(s.contains("are"));
+		
+		
+		// there is an "are" AND it starts at index 10
+		System.out.println(s.indexOf("are"));
+		
+		
+		// if not there u will get a -1
+		System.out.println(s.indexOf("tarzan"));
+		
+		/*
 		
 		// concatenate string
 		
@@ -24,7 +47,7 @@ public class Driver {
 		b = s.getBytes();
 		
 		System.out.println((char)b[0]);
-		
+		*/
 		
 
 		
--- Java/variables/src/variables/Driver.java
+++ Java/variables/src/variables/Driver.java
@@ -10,10 +10,10 @@ public class Driver {
 		// behave like C++ except
 		// cannot have pointers to primitive variables
 		
-		int x = 10;
+		int x = 10; // this a primitive!
 		double y = 20.0;
-		System.out.println(x);
-		System.out.println(y);
+		//System.out.println(x);
+		//System.out.println(y);
 		
 		// int *p;  //cannot do this
 		
@@ -22,20 +22,64 @@ public class Driver {
 		// Everything else is a Class variable.
 	    
 		Integer i;   // same as int * i;
+		//Integer i is just an address variable. 
+		//i is an Address (pointer to) of an Integer.
 		
 		i = new Integer(100); // i = new int(100)
 		
+		i = 200; // this does not make sense. i is an address?
+		         // secretly does i = new Integer(200).
+		
+		
 		System.out.println(i);
 		
-		/*
+		
+		i = x;  // same problem address = int xx
+		        // secretly the system did i = new Integer(x)
+		
+		x = i;  // int x = address + 1   *i++;
+		
+		          // secretly does this x = i.valueof() + 1 
+		
+		System.out.println(x);
+		
+		double myDouble = 9.78;
+	    int j = (int) myDouble;
+	    
+	    System.out.println(j);
+		
+	
 		//converting between class and primitive variables
 		
 	
 		// using valueof
-		i = Integer.valueOf("231");
+		// Integer is a Class. 
+		
+		// Cat.jump()   /// did you do this in C++?
+		
+		// Cat c = new Cat();
+		// c.jump();
+		
+		
+		i = Integer.valueOf("23183838");
+		
+		// write a program to take "123" and convert it to 
+		// the number 123
+		
+		System.out.println(i);
+		
 		i = Integer.parseInt("123");
 		
-		*/
+		// Big difference with respect to boolean.
+		// in Java Boolean is a Boolean and NOT an integer
+		
+		boolean isJavaFun = true;
+	    boolean isFishTasty = false;
+	    
+	    System.out.println(isJavaFun);     // Outputs true
+	    System.out.println(isFishTasty);   // Outputs false
+	    
+		
 
 	}
 
