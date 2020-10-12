3514c436c8ce2285afa02518a50d448cde9e3a6b
izualkernan <izualkernan@aus.edu>
2020-10-12 10:29:51 +0400

    Oct 11, 2020

--- Threads/Thread1/src/Driver.java
+++ Threads/Thread1/src/Driver.java
@@ -3,8 +3,19 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		num n1 = new num();
-		num n2 = new num();
+		
+		// create two threads n1, n2
+		// each will print numbers. 
+		
+		//num n1 = new num();
+		//num n2 = new num();
+		
+		// save the address of each thread. 
+		
+		 num [] nums = new num[1000];
+		 
+		for (int i=0; i<1000; i++)
+			nums[i] = new num();
 
 	}
 
--- Threads/Thread1/src/num.java
+++ Threads/Thread1/src/num.java
@@ -1,12 +1,19 @@
 import java.util.Random;
 
 public class num extends Thread {
+ 
 Random r = new Random();
 
 num (){
 	this.start();  //actually start the thread
+	
+	// start() does: 
+	// 1.  Do some bookkeeping.
+	// 2.  Call the run() function. 
 }
+
 // my behavior -- DO NOT CALL run() directly.
+// run() is the main() of each thread.
 
 public void run() {
  
@@ -15,6 +22,9 @@ public void run() {
 	 System.out.println("Thread "+this+" generated "+r.nextLong());
 	 
 	 try {
+		
+		// sleep for 1 second
+		 
 		Thread.sleep(1000);
 	} catch (InterruptedException e) {
 		// TODO Auto-generated catch block
