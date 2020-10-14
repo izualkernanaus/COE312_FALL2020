c95620ba268e688c00a22e07f7b85f691bd132cd
izualkernan <izualkernan@aus.edu>
2020-10-14 10:37:19 +0400

    Oct 14 2020

--- Threads/Thread3/src/Driver.java
+++ Threads/Thread3/src/Driver.java
@@ -7,10 +7,24 @@ public class Driver {
 		print n2 = new print(15);
 
 		
+		// give me the thread which is running n1
+		// I (main) will wait until n1 finishes running. 
 		n1.get_thread().join();
-		n2.get_thread().join();
+		
+		// give me the thread that is running n2
+		// I (main) will wait until n2 finishes running.
+		// n2.get_thread().join();
 		
 		System.out.println("Hasta Lavista Baby!");
+		
+		// how we grab the thread of ourselves 
+		
+		// this will lock for ever 
+		
+		// Thread.currentThread().join();
+		
+		// After the join
+		System.out.println("main after the join");
 
 
 	}
